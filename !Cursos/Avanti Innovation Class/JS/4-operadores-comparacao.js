05c1fe654149d91845f68d667d6631c0fc1edbbd
Rerigan <cosnnum@gmail.com>
2025-04-21 19:15:33 -0300

    hm

---
+++ !Cursos/Avanti Innovation Class/JS/4-operadores-comparacao.js
@@ -0,0 +1,15 @@
+const numero1 = 5
+const numero2 = 5
+const numero3 = 3
+// == igualdade 
+// === igualdade restrita (para mesmos tipos de dados)
+// != diferente 
+// !== diferença restrita (mesmo tipo de dados)
+// > maior 
+// < menor 
+// >= maior igual 
+// <= menor igual
+const resultado = numero1 > numero2
+
+
+console.log(`O resultado é ${resultado}`)
