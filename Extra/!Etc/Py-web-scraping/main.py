8c36da3f6501394e5ab0403f52d680e6ea6ec0ad
Rerigan <cosnnum@gmail.com>
2025-05-04 19:22:38 -0300

    web-scraping-test

--- Extra/!Etc/Py-web-scraping/main.py
+++ Extra/!Etc/Py-web-scraping/main.py
@@ -1,9 +1,9 @@
 class Bot();
     
-    def __init__(self): None:
+    def __init__(self, url) -> None:
         pass
 
 
 if __name__ == '__main__':
-    url = 'https://br.indeed.com/jobs?q=desenvolvedor&l=londrina%2C+pr'
+    url = 'https://br.indeed.com/jobs?q=est%C3%A1gio&l=londrina%2C+pr'
     bot = Bot(url)
